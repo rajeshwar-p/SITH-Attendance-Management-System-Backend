efda094ce0c966cec15d5a7eb97a3da886e771d7
rajeshwar <rajeshwarprajapati303@gmail.com>
2026-05-17 18:38:23 +0530

    Added CORS origin

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ dotenv.config();
 const app = express();
 
 // ✅ MIDDLEWARE
-app.use(cors());
+app.use(cors({
+  origin: "https://sith-attendance-management-system.vercel.app",
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
